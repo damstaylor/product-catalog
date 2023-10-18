d3028ab8a878270d64380c6751b886232f5a6d48
Damien <damiensabo@live.fr>
2023-10-18 12:55:25 +0200

    Add ProductList.vue component with mock product data

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,36 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <v-app id="inspire">
+    <v-app-bar app class="secondary">
+      <v-toolbar-title class="nav-bar--title">STRAPI APP</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
+    <v-main class="blue lighten-2">
+      <ProductList :products="products" />
     </v-main>
+    <v-footer></v-footer>
   </v-app>
 </template>
 
 <script>
+import ProductList from "@/components/ProductList";
+import products from "../mocks/products.json";
+
 export default {
   name: 'App',
-
   components: {
+    ProductList,
   },
-
+  created() {
+    this.products = products;
+  },
   data: () => ({
-    //
+    products: [],
   }),
 };
 </script>
+
+<style scoped>
+  .nav-bar--title {
+    color: white;
+  }
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="center">
+    <v-card class="ma-2" v-for="(product, index) in products" :key="index" max-width="270">
+      <v-card-title>{{ product.name }}</v-card-title>
+      <v-card-text>
+        <strong>
+          {{ getPrice(product) }}
+        </strong>
+      </v-card-text>
+    </v-card>
+  </v-row>
+</template>
+
+<script>
+import {defineComponent} from "vue"
+
+export default defineComponent({
+  name: "ProductList",
+  props: {
+    products: { type: Array, default: () => [] },
+  },
+  data: () => ({
+    apiUrl: "",
+  }),
+  computed: {
+    getPrice() {
+      return product => product.unit_price ? `${product.unit_price},00€` : "";
+    },
+  },
+})
+</script>
